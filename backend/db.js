16cd8b62fc6ff7784bb1e41460fcd42eb7cf3837
QamarJaradat <moon19.qs@gmail.com>
2020-11-12 11:50:17 +0200

    init frontend

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 var url = "mongodb+srv://lef:1234@cluster0.diesu.mongodb.net/<dbname>?retryWrites=true&w=majority"
-mongoose.connect(url)
+mongoose.connect(url, { useUnifiedTopology: true, useNewUrlParser: true })
 var db = mongoose.connection
-db.on('error',console.error.bind(console,'connection error'))
-db.once('open',function(){
+db.on('error', console.error.bind(console, 'connection error'))
+db.once('open', function () {
     console.log('connection to db sucessful')
 })
 module.exports = db
