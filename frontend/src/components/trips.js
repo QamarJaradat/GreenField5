894638e2664c0a716ce031adeea6d864edf20c60
QamarJaradat <moon19.qs@gmail.com>
2020-11-14 13:54:59 +0200

    little changes

--- frontend/src/components/trips.js
+++ frontend/src/components/trips.js
@@ -8,9 +8,12 @@ class Trip extends React.Component {
     constructor(props) {
         super(props)
         this.state = {}
+        this.booktrip = this.booktrip.bind(this)
     }
 
-
+    booktrip() {
+        console.log('clicked')
+    }
     render() {
         return (
             <div>
@@ -27,7 +30,11 @@ class Trip extends React.Component {
 
                     <div>
                         <img className='imgs' src='https://www.flaticon.com/svg/static/icons/svg/1071/1071526.svg' alt='People'></img>
-                        <p>max number of people</p>
+                        <p>max number of people </p>
+                    </div>
+                    <div>
+                        <img className='imgs' src='https://www.flaticon.com/svg/static/icons/svg/2635/2635433.svg' alt='Price'></img>
+                        <p>Price</p>
                     </div>
 
                     <div>
@@ -39,46 +46,49 @@ class Trip extends React.Component {
                 <br></br>
                 <div className='d-flex flex-wrap justify-content-around'>
 
-                    <div className='inlinediv' style={{ 'width': '70%', 'height': '200px' }}>
+                    <div className='inlinediv' style={{ 'width': '55%', 'height': '200px' }}>
                         <div style={{ 'width': '99%', 'height': "30px", 'backgroundColor': "#ad679d", 'color': 'white' }}>
                             <h5 style={{ 'marginLeft': '15px' }}>Day1</h5>
                         </div>
 
-                        <p style={{ 'marginTop': '8px' }}>we will start our trip be visiting The Terraces of the Baháʼí Faith
+                        <p className='pfont' style={{ 'marginTop': '8px' }}>we will start our trip be visiting The Terraces of the Baháʼí Faith
                         These gardens extend over Mount Carmel. The harbor, this garden in the Baha'i World Center,
                         includes a building with a golden dome, and it is the duty of a Baha'i missionary who was killed
                         in 1850, and his remains were transferred to Haifa.
                         You will enjoy an Awesome view of the city from the top of the hill.</p>
 
                     </div>
-                    <div className='inlinediv' style={{ 'width': '27%', 'height': '200px' }}>
+                    <div className='inlinediv' style={{ 'width': '44%', 'height': '200px' }}>
                         <img className='tripimgs' src='https://4.bp.blogspot.com/-eapu1aATAhk/VOn6pl4bvqI/AAAAAAAAOnU/JywtXHmyloQ/s1600/Baha%E2%80%99i%2BShrine%2Band%2BGardens%2BThe%2Bholy%2Bplace%2Bof%2BBaha%E2%80%99i%2BFaith-1.jpg' alt='Date'></img>
                     </div>
                 </div>
 
                 <div className='d-flex flex-wrap justify-content-around'>
 
-                    <div className='inlinediv' style={{ 'width': '70%', 'height': '200px' }}>
+                    <div className='inlinediv' style={{ 'width': '55%', 'height': '200px' }}>
                         <div style={{ 'width': '99%', 'height': "30px", 'backgroundColor': "#ad679d", 'color': 'white' }}>
                             <h5 style={{ 'marginLeft': '15px' }}>Day1</h5>
                         </div>
 
-                        <p style={{ 'marginTop': '8px' }}>we will start our trip be visiting The Terraces of the Baháʼí Faith
+                        <p className='pfont' style={{ 'marginTop': '8px' }}>we will start our trip be visiting The Terraces of the Baháʼí Faith
                         These gardens extend over Mount Carmel. The harbor, this garden in the Baha'i World Center,
                         includes a building with a golden dome, and it is the duty of a Baha'i missionary who was killed
                         in 1850, and his remains were transferred to Haifa.
                         You will enjoy an Awesome view of the city from the top of the hill.</p>
 
                     </div>
-                    <div className='inlinediv' style={{ 'width': '27%', 'height': '200px' }}>
+                    <div className='inlinediv' style={{ 'width': '44%', 'height': '200px' }}>
                         <img className='tripimgs' src='https://4.bp.blogspot.com/-eapu1aATAhk/VOn6pl4bvqI/AAAAAAAAOnU/JywtXHmyloQ/s1600/Baha%E2%80%99i%2BShrine%2Band%2BGardens%2BThe%2Bholy%2Bplace%2Bof%2BBaha%E2%80%99i%2BFaith-1.jpg' alt='Date'></img>
                     </div>
                 </div>
 
 
+
+
+
                 <div>
                     <p align="right" style={{ 'marginRight': '12px' }}>
-                        <input className='btn btn-primary' type="button" value="Book this trip" />
+                        <input className='btn btn-primary' type="button" value="Book this trip" onClick={this.booktrip} />
                     </p>
                 </div>
             </div>
