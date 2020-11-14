0d75d7b1fc84c0e31618a37daa81baf127f5a492
rzanabusamra22 <rzanabusamra@gmail.com>
2020-11-14 17:44:20 +0200

    Trips cards main page

--- frontend/src/components/Carditem.js
+++ frontend/src/components/Carditem.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-
-function Carditem() {
+function CardItem(props) {
     return (
         <>
             <li className='cards__item'>
-                <Link className="cards__item__link">
-                    <figure className="cards__item__pic-wrap">
-                        <img src="" alt="Trip image" className="cards__item__img" />
+                <Link className='cards__item__link' to={props.path}>
+                    <figure className='cards__item__pic-wrap' data-category={props.label}>
+                        <img
+                            className='cards__item__img'
+                            alt='Travel Image'
+                            src={props.src}
+                        />
                     </figure>
-                    <div className="cards__item__info">
-                        <h5 className="cards__item__text"></h5>
+                    <div className='cards__item__info'>
+                        <h5 className='cards__item__text'>{props.text}</h5>
                     </div>
                 </Link>
             </li>
         </>
-    )
+    );
 }
 
-export default Carditem
+export default CardItem;
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../App.css';
+import Cards from './Cards';
 import Section from './Section';
 
 function Home() {
     return (
         <>
             <Section />
+            <Cards />
         </>
     )
 }
