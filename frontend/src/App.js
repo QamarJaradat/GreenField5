478f4c4ca85b5b3a1b7e349746f8275dcc3f585a
QamarJaradat <moon19.qs@gmail.com>
2020-11-15 23:15:34 +0200

    apply css on payment page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import Trips from './components/Homepage/Cards'
 import Login from './components/user/login'
 import Trip from './components/trips/trips'
 import Signup from './components/user/signup'
+import Payment from './components/payment/payment'
 
 
 
@@ -40,7 +41,7 @@ class App extends React.Component {
                 render={(props) => <Signup toggleLogin={this.changeLogInStatus} />}
               />
 
-              <Route path="/" exact component={Home} />
+              <Route path="/" exact component={Payment} />
               <Route path="/trips" exact component={Trips} />
               {/* <Route path="/sign-up" exact component={Signup} /> */}
               <Route path="/user" exact component={Profile} />
@@ -60,7 +61,7 @@ class App extends React.Component {
           <Router>
             <Navbar />
             <Switch>
-              <Route path="/" exact component={Home} />
+              <Route path="/" exact component={Payment} />
               <Route path="/trips" exact component={Trip} />
               <Route
                 path='/sign-up'
