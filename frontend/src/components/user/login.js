bf477565ac7c0e678ed64a0868f99555e97efe8d
QamarJaradat <moon19.qs@gmail.com>
2020-11-15 13:44:58 +0200

    change of the layout for login page

--- frontend/src/components/user/login.js
+++ frontend/src/components/user/login.js
@@ -4,30 +4,42 @@ import './login.css';
 class Login extends Component {
   render() {
     return (
-    <div className="test">
+      <div className="test">
 
-      <div className="row">
+        <div className="row">
 
-        <div id="signin" className="col-sm-4 left form-group">
-          <form action="#">
-            <h1>do you have an acount please sign in here</h1>
-            <input type="email" className="form-control" name="email" placeholder="moon19 allah"/>
-            <input type="passowrd" className="form-control" name="password" placeholder="put the password or die"/>
 
-          </form>
-        </div>
-        <div id="signup" class="col-sm-4 right">
-          <form action="#">
-            <h1>new to our website join us and signup here</h1>
-            <input type="string" className="form-control" name="firstName" placeholder="First Name"/>
-            <input type="string" className="form-control" name="lastName" placeholder="Last Name"/>
-            <input type="email" className="form-control" name="email" placeholder="moon19 allah"/>
-            <input type="passowrd" className="form-control" name="password" placeholder="put the password or die"/>
-            <input type="passowrd" className="form-control" name="conformPassword" placeholder="do it again"/>
-          </form>
+          <div id="signup" class="col-sm-4 right" >
+            <form action="#" className='form1' >
+              <h1>new to our website join us and signup here</h1>
+              <div>
+                <label>First Name</label>
+                <input type="string" className="form-control" name="firstName" placeholder="First Name" />
+              </div>
+              <div>
+                <label>last Name</label>
+                <input type="string" className="form-control" name="lastName" placeholder="Last Name" />
+              </div>
+              <div>
+                <label>Email</label>
+                <input type="email" className="form-control" name="email" placeholder="moon19 allah" />
+              </div>
+              <div>
+                <label>Password</label>
+                <input type="passowrd" className="form-control" name="password" placeholder="put the password or die" />
+              </div>
+              <div>
+                <lable>Confirm Password</lable>
+                <input type="passowrd" className="form-control" name="conformPassword" placeholder="do it again" />
+              </div>
+              <div style={{ "marginTop": '12px' }}>
+                <input type='button' value='Sign Up!' className="btn btn-secondary" style={{ "display": 'inline-block', "marginRight": '10px' }}></input>
+                <small id="emailHelp" class="form-text text-muted" style={{ "display": 'inline-block' }}>have account? Login.</small>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
     )
   }
 }
---
+++ frontend/src/components/user/signup.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import './login.css';
+
+class Login extends Component {
+    render() {
+        return (
+            <div className="test">
+
+                <div className="row">
+
+                    <div id="signin" className="col-sm-4 left form-group">
+                        <form action="#">
+                            <h1>do you have an acount please sign in here</h1>
+                            <input type="email" className="form-control" name="email" placeholder="moon19 allah" />
+                            <input type="passowrd" className="form-control" name="password" placeholder="put the password or die" />
+
+                        </form>
+                    </div>
+
+                </div>
+            </div>
+        )
+    }
+}
+
+
+
+export default Login;
