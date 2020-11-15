4481ff8e7bd411052c852590d6e57ab8806f7b81
unknown <noorashqar123@gmail.com>
2020-11-15 19:39:45 +0200

    signin-signup switch

--- frontend/src/components/user/signup.js
+++ frontend/src/components/user/signup.js
@@ -1,28 +1,61 @@
 import React, { Component } from 'react'
 import './login.css';
 
-class Login extends Component {
-    render() {
-        return (
-            <div className="test">
-
-                <div className="row">
-
-                    <div id="signin" className="col-sm-4 left form-group">
-                        <form action="#">
-                            <h1>do you have an acount please sign in here</h1>
-                            <input type="email" className="form-control" name="email" placeholder="moon19 allah" />
-                            <input type="passowrd" className="form-control" name="password" placeholder="put the password or die" />
-
-                        </form>
-                    </div>
-
-                </div>
-            </div>
-        )
+class Signup extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+        islogin : false,
     }
+    this.changeLogInStatus = this.changeLogInStatus.bind(this)
+} 
+changeLogInStatus(){
+  this.setState({
+    islogin : !this.state.islogin
+  })
+}
+  render() {
+    return (
+      <div className="test">
+
+        <div className="row">
+
+
+          <div id="signup" className="col-sm-4 right" >
+            <form action="#" className='form1' >
+              <h1>new to our website join us and signup here</h1>
+              <div>
+                <label>First Name</label>
+                <input type="string" className="form-control" name="firstName" placeholder="First Name" />
+              </div>
+              <div>
+                <label>Last Name</label>
+                <input type="string" className="form-control" name="lastName" placeholder="Last Name" />
+              </div>
+              <div>
+                <label>Email</label>
+                <input type="email" className="form-control" name="email" placeholder="moon19 allah" />
+              </div>
+              <div>
+                <label>Password</label>
+                <input type="passowrd" className="form-control" name="password" placeholder="put the password or die" />
+              </div>
+              <div>
+                <lable>Confirm Password</lable>
+                <input type="passowrd" className="form-control" name="conformPassword" placeholder="do it again" />
+              </div>
+              <div style={{ "marginTop": '12px' }}>
+                <input type='button' value='Sign Up!' className="btn btn-secondary" style={{ "display": 'inline-block', "marginRight": '10px' }}></input>
+                <small id="emailHelp" className="form-text text-muted" style={{ "display": 'inline-block' }} onClick = {this.props.toggleLogin}>have account? Login.</small>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 
 
-export default Login;
+export default Signup;
