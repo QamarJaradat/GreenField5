334c9bf06bc21c0d443f1b8b2a73b08a63aec680
rzanabusamra22 <rzanabusamra@gmail.com>
2020-11-19 11:42:33 +0200

    user backend

--- routes.js
+++ routes.js
@@ -5,12 +5,12 @@ const tripController = require('./Controller/TripsController')
 const userController = require('./Controller/UserController')
 routers.post('/signup', userController.signUpUser);
 routers.post('/login', userController.loginUser);
-routers.post('/logout', userController.userlogout)
-
+routers.post('/logout', userController.userlogout);
+routers.get('/user', userController.userData);
 //Payment 
 const paymentController = require('./Controller/PaymentController')
 routers.post('/payment', paymentController.payment)
-
+//Trips 
 routers.get('/gettrips', tripController.tripsList)
 routers.post('/gettrips', tripController.updateTrip)
 routers.get('/filldata', tripController.fillTrips)
